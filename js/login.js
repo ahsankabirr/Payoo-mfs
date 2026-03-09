3daedb015ea966e2b90a7a4a4a846866db5eae9e
ahsankabirr <mdahsankabirrazu@gmail.com>
2026-03-09 16:00:35 +0200

    login section add

--- js/login.js
+++ js/login.js
@@ -1,5 +1,14 @@
 document.getElementById("button-login").addEventListener("click", (event) => {
-  const phoneNumber = document.getElementById("number-Text");
-  const pinNumber = document.getElementById("Pin-Number");
-  console.log(phoneNumber.value);
+  const phoneNumber = document.getElementById("number-Text").value;
+  const pinNumber = document.getElementById("Pin-Number").value;
+  event.preventDefault();
+  console.log(phoneNumber, pinNumber);
+
+  if (phoneNumber === "0130457777" && pinNumber === "1234") {
+    window.location.href = "/home.html";
+
+    console.log("Your logged in");
+  } else {
+    console.log("Wrong phone number of pin");
+  }
 });
