92194836551bceb8309f83b403c4db0589703c4f
ahsankabirr <mdahsankabirrazu@gmail.com>
2026-03-09 14:14:05 +0200

    add js section

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("button-login").addEventListener("click", (event) => {
+  const phoneNumber = document.getElementById("number-Text");
+  const pinNumber = document.getElementById("Pin-Number");
+  console.log(phoneNumber.value);
+});
